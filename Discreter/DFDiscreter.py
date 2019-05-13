b0a35131c251d7a6d464d0e95e82b26fb453df80
魏小豪 <jackweixh@foxmial.com>
2019-05-13 17:20:17 +0800

    添加等宽、等频离散化方法

--- Discreter/DFDiscreter.py
+++ Discreter/DFDiscreter.py
@@ -1,3 +1,4 @@
+import pandas as pd
 
 
 def points():
@@ -8,23 +9,33 @@ def points():
     """
 
 
-def equal_freq():
+def freq(col_data: pd.DataFrame, n_bins: int, labels=None) -> pd.DataFrame:
     """
     等频分箱，各离散值拥有相等数量的样本
 
-    :return:
+    :param col_data: 需要离散化的列数据
+    :param n_bins: 要求划分成的类别个数
+    :param labels: 是否将离散化后的类别映射成 序号索引，默认 True
+    :return: 离散化后 DataFrame 数据
     """
 
+    return pd.qcut(col_data, n_bins, labels=labels)
 
-def equal_width():
+
+def width(col_data: pd.DataFrame, n_bins: int, labels=None) -> pd.DataFrame:
     """
     等宽分箱，将取值区间均分为若干个子区间
 
-    :return:
+    :param col_data: 需要离散化的列数据
+    :param n_bins: 要求划分成的类别个数
+    :param labels: 是否将离散化后的类别映射成 序号索引，默认 True
+    :return: 离散化后 DataFrame 数据
     """
 
+    return pd.cut(col_data, n_bins, labels=labels)
 
-def hash_bins():
+
+def hashtable():
     """
     Hash分桶，利用 Hash table 方法离散数据
 
