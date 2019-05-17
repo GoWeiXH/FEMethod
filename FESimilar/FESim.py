209039de80458531f8d46201b057369da15427f1
GoWeiXH <jackweixh@126.com>
2019-05-17 17:27:18 +0800

    列出计算相似度的的方法以备添加

---
+++ FESimilar/FESim.py
@@ -0,0 +1,76 @@
+
+
+def jaccard(data_1, data_2):
+    """
+    Jaccard 系数
+
+    :return:
+    """
+
+
+def dis_l2(data_1, data_2):
+    """
+    欧氏距离
+
+    :return:
+    """
+
+
+def dis_l1(data_1, data_2):
+    """
+    曼哈顿距离
+
+    :return:
+    """
+
+
+def pearson(data_1, data_2):
+    """
+    Pearson 系数
+
+    :return:
+    """
+
+
+def spearman(data_1, data_2):
+    """
+    Spearman 相关系数
+
+    :param data_1:
+    :param data_2:
+    :return:
+    """
+
+
+def kendall(data_1, data_2):
+    """
+    Kendall Rank 相关系数
+
+    :param data_1:
+    :param data_2:
+    :return:
+    """
+
+
+def cosine(data_1, data_2):
+    """
+    余弦相似度
+
+    :return:
+    """
+
+
+def cross_entropy(data_1, data_2):
+    """
+    交叉熵
+
+    :return:
+    """
+
+
+def kl_div(data_1, data_2):
+    """
+    K-L 散度
+
+    :return:
+    """
